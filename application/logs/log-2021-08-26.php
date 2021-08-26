50544d5a5fd852deb594e4c338ee6cad5f205160
hikmat20 <hikmataulia20@gmail.com>
2021-08-26 13:33:58 +0700

    log

---
+++ application/logs/log-2021-08-26.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-08-26 04:38:18 --> 404 Page Not Found: /index
+ERROR - 2021-08-26 04:38:28 --> 404 Page Not Found: /index
