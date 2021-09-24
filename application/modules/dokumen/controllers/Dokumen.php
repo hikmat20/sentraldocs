c4f0ffb2088daeb206c9c8fcde37dfacb0724949
hikmat20 <hikmataulia20@gmail.com>
2021-09-24 21:54:15 +0700

    nama file

--- application/modules/dokumen/controllers/Dokumen.php
+++ application/modules/dokumen/controllers/Dokumen.php
@@ -316,14 +316,14 @@ class Dokumen extends Admin_Controller
 
 	public function confirm_download($id, $table)
 	{
-		echo '<pre>';
-		print_r($id . ", " . $table);
-		echo '<pre>';
-		exit;
-
 		$session 	= $this->session->userdata('app_session');
 		$dist 		= $this->db->get_where('distribusi', ['id_file' => $id])->result();
 		$nama_file 	= $this->db->get_where($table, ['id' => $id])->row();
+
+		echo '<pre>';
+		print_r($nama_file);
+		echo '<pre>';
+		exit;
 
 		if (!$nama_file) {
 			$return = [
