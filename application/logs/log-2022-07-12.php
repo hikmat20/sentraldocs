b7796ea9f3ae2dfb2212693a34a3771e191f7390
hikmat20 <hikmataulia20@gmail.com>
2022-07-12 10:25:19 +0700

    update link dashboard

---
+++ application/logs/log-2022-07-12.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-07-12 10:22:30 --> 404 Page Not Found: /index
+ERROR - 2022-07-12 10:22:30 --> 404 Page Not Found: /index
+ERROR - 2022-07-12 10:22:41 --> 404 Page Not Found: /index
