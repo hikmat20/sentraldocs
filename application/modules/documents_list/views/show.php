b24c509de7350aa15a5923fcc24be5ce2a11d22f
ssc <itprogrammerssc2020@gmail.com>
2022-08-29 10:56:57 +0700

    add permission

--- application/modules/documents_list/views/show.php
+++ application/modules/documents_list/views/show.php
@@ -21,8 +21,11 @@
 <div class="tab-content mt-5">
     <div class="tab-pane fade show active" id="file" role="tabpanel" aria-labelledby="file-tab">
         <div style="width:92%;height:400px;background-color: red;position: absolute;opacity: 0;"></div>
-        <iframe src="https://docs.google.com/gview?embedded=true&url=<?= base_url("directory/$dir_name/$file->file_name"); ?>&rm=minimal#toolbar=0&navpanes=0" frameborder="0" width="100%" height="400px"></iframe>
-
+<?php if($file->ext == '.pdf' || $file->ext == '.PDF'): ?>
+        <iframe src="<?= base_url("directory/$dir_name/$file->file_name"); ?>#toolbar=0&navpanes=0" frameborder="0" width="100%" height="400px"></iframe>
+<?php else : ?>
+<iframe src="https://docs.google.com/gview?embedded=true&url=<?= base_url("directory/$dir_name/$file->file_name"); ?>&rm=minimal#toolbar=0&navpanes=0" frameborder="0" width="100%" height="400px"></iframe>
+<?php endif; ?>
         <hr>
     </div>
     <div class="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">
