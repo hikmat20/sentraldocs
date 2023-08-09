6b83d58a27ccdc938c8ebf9694bd86d6cca29822
hikmat20 <hikmataulia20@hmail.com>
2023-08-09 08:32:21 +0700

    update materi

--- application/modules/materi/views/index.php
+++ application/modules/materi/views/index.php
@@ -393,7 +393,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modalId" tabindex="-1" data-backdrop="static" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-	<div class="modal-dialog modal-dialog-scrollable" role="document">
+	<div class="modal-dialog modal-dialog-scrollable" style="max-width: 100%;" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
 				<h5 class="modal-title"></h5>
