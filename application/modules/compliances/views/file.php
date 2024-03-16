fa3d3ac85f81dd2e2b185e3a84e65568e8145871
hikmat20 <hikmataulia20@gmail.com>
2024-03-16 14:20:26 +0700

    fix:file not load in compliances detail

--- application/modules/compliances/views/file.php
+++ application/modules/compliances/views/file.php
@@ -5,7 +5,7 @@
 		padding: 0;
 	}
 </style>
-<iframe id="file" src="/directory/COMPLIANCE/<?= $comp ; ?>/<?= $file; ?>#toolbar=0&navpanes=0&scrollbar=0" oncontextmenu="return false;" frameborder="0" width="100%" height="100%">
+<iframe id="file" src="<?= base_url($this->uri->segment(1)) ; ?>/directory/COMPLIANCE/<?= $comp ; ?>/<?= $file; ?>#toolbar=0&navpanes=0&scrollbar=0" oncontextmenu="return false;" frameborder="0" width="100%" height="100%">
 	<script>
 		window.frames["file"].contentDocument.oncontextmenu = function() {
 			return false;
